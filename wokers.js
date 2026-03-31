675275798bb25606e08a3eb705ecc72b6a4ff456
ibrahim9071 <infinitypo35@gmail.com>
2026-03-31 08:43:04 +0300

    Update wokers.js

--- wokers.js
+++ wokers.js
@@ -1,33 +1,27 @@
 export default {
   async fetch(request) {
-    const BASE_URL = "https://trox.cinemax6.com:3330/stream/";
     const url = new URL(request.url);
-
-    let targetUrl;
-
-    if (url.pathname === "/" || url.pathname.endsWith(".m3u8")) {
-      targetUrl = BASE_URL + "play.m3u8";
-    } else {
-      targetUrl = BASE_URL + url.pathname.split("/").pop();
-    }
-
+    const targetBase = "https://trox.cinemax6.com:3330";
+    
+    // Gelen path'i hedefe ekle
+    const targetUrl = targetBase + url.pathname;
+    
     const response = await fetch(targetUrl, {
       headers: {
         "User-Agent": "Mozilla/5.0",
-        "Referer": "https://trox.cinemax6.com/"
+        "Origin": targetBase,
+        "Referer": targetBase + "/"
       }
     });
-
-    let contentType = response.headers.get("content-type");
-
-    if (contentType.includes("application/vnd.apple.mpegurl")) {
+    
+    // Eğer m3u8 dosyasıysa, içindeki segment URL'lerini değiştir
+    if (url.pathname.endsWith('.m3u8')) {
       let text = await response.text();
-
-      // TS linklerini yeniden yaz
-      text = text.replace(/(.*\.ts)/g, (match) => {
-        return request.url.replace(/\/[^\/]*$/, "/" + match);
+      // Segment linklerini worker'ın üzerinden geçir
+      text = text.replace(/^(?!https?:\/\/)(.+\.ts)/gm, (match) => {
+        return `https://${url.hostname}/${match}`;
       });
-
+      
       return new Response(text, {
         headers: {
           "Content-Type": "application/vnd.apple.mpegurl",
@@ -35,12 +29,7 @@ export default {
         }
       });
     }
-
-    return new Response(response.body, {
-      headers: {
-        "Content-Type": contentType,
-        "Access-Control-Allow-Origin": "*"
-      }
-    });
+    
+    return response;
   }
-};
+}
